75568ad969fac2a1615cc3fd20ccf430120c5435
maiconm <maiconm23@gmail.com>
2020-08-04 23:18:57 -0300

    Landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 import './assets/styles/global.css';
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Landing />
   );
 }
 
