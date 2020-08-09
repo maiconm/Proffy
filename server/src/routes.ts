94fe64ac07487152c9339e4f695c264255f1cec4
maiconm <maiconm23@gmail.com>
2020-08-09 13:10:26 -0300

    create class route

---
+++ server/src/routes.ts
@@ -0,0 +1,66 @@
+import express from 'express'
+import db from './database/connection'
+import convertHourToMinute from './utils/convertHourToMinute'
+
+const routes = express.Router()
+
+interface ScheduleItem {
+  week_day: number
+  from: string
+  to: string
+}
+
+routes.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body
+
+  const trx = await db.transaction()
+
+  try {
+    const insertedUsersIds = await <Promise<any>>trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+    })
+    
+    const user_id = insertedUsersIds[0]
+  
+    const insertedClassesId = await <Promise<any>>trx('classes').insert({
+      subject,
+      cost,
+      user_id
+    })
+  
+    const class_id = insertedClassesId[0]
+  
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinute(scheduleItem.from),
+        to: convertHourToMinute(scheduleItem.to),
+      }
+    })
+  
+    await <Promise<any>>trx('class_schedule').insert(classSchedule)
+  
+    await <Promise<any>>trx.commit()
+  
+    return response.status(201).send()
+  } catch (err) {
+    await <Promise<any>>trx.rollback()
+    return response.status(400).json({
+      error:`unexpect error while creating new class: ${err}`
+    })
+  }
+})
+
+export default routes
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-  const users = [
-    { name: 'maicon', age: 23 },
-    { name: 'diego', age: 25 },
-  ]
-  return response.json(users)
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
