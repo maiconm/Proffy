cfe8f7a98e39467bf63968f8b1a17a55e5f98ef9
maiconm <maiconm23@gmail.com>
2020-08-10 23:01:55 -0300

    Landing page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { AppLoading } from 'expo'
+import { useFonts, Archivo_400Regular, Archivo_700Bold, Archivo_600SemiBold } from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+
+import Landing from './src/pages/Landing'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello NLW</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Archivo_600SemiBold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="auto" />
+      </>
+    )
+  }
+}
