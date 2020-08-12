263c0ce8bf4110a2a078f3c34769121f1c7c80db
maiconm <maiconm23@gmail.com>
2020-08-11 22:16:48 -0300

    Tabs navigation

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -16,6 +16,10 @@ function Landing() {
     navigate('GiveClasses')
   }
 
+  function handleNavigateToStudyPages() {
+    navigate('Study')
+  }
+
   return (
     <View style={styles.container}>
       <Image style={styles.banner} source={landingImg}/>
@@ -25,7 +29,10 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
