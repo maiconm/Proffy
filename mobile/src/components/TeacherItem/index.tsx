1fbeec890e974879737453e5bfedb7f7f36b2420
maiconm <maiconm23@gmail.com>
2020-08-12 23:31:19 -0300

    Teacher item component

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { View, Image, Text } from 'react-native'
+
+import styles from './styles'
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
+import { RectButton } from 'react-native-gesture-handler'
+function TeacherItem() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://github.com/maiconm.png' }}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Maicon Andraski</Text>
+          <Text style={styles.subject}>Quimica</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Entusiasta das melhores tecnologias de quimica avancada.
+        {'\n'}{'\n'}
+        Apaixonado por explodir coisas em laboratorio e por mudar a vida das pessoes atraves de experiencias.
+      </Text>
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preco/hora {'  '}
+          <Text style={styles.priceValue}>R$ 20,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartOutlineIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default TeacherItem
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -3,11 +3,19 @@ import { View } from 'react-native'
 
 import styles from './styles'
 import PageHeader from '../../components/PageHeader'
+import TeacherItem from '../../components/TeacherItem'
+import { ScrollView } from 'react-native-gesture-handler'
 
 function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponives"/>
+      <ScrollView>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   )
 }
